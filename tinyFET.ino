d69a46a99ff40258f20483e660a80b4e0f1fe55e
Peter Barker <moosepr@gmail.com>
2017-09-19 21:17:40 +0100

    Add files via upload
    added i2c functions maybe

--- tinyFET.ino
+++ tinyFET.ino
@@ -1,6 +1,7 @@
 #include <avr/sleep.h>
 #include <avr/interrupt.h>
 #include <Adafruit_NeoPixel.h>
+#include <TinyWireS.h>
 //some stolen constants defines for usage - http://www.technoblogy.com/show?KX0
 #define adc_disable() (ADCSRA &= ~(1<<ADEN)) // disable ADC (before power-off)
 #define adc_enable()  (ADCSRA |=  (1<<ADEN)) // re-enable ADC
@@ -9,33 +10,40 @@
 #define MOSFETpin 3
 #define PixelPin 4
 #define bright 25
+#define I2C_SLAVE_ADDRESS 0xE3//this is hex for 227, which is the ascii code for pi :P
 
 int pressCount,colourCount,voltage;
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(1, PixelPin, NEO_GRB + NEO_KHZ800);
 
 
 void setup() {
+  //init the i2c
+  TinyWireS.begin(I2C_SLAVE_ADDRESS); 
+  TinyWireS.onReceive(receiveEvent); 
+  TinyWireS.onRequest(requestEvent);
   //init the neopixel
   pixels.begin();
   pixels.setBrightness(25);
   pixels.show();
   pinMode(pressPin, INPUT_PULLUP);
   pinMode(MOSFETpin,OUTPUT);
-  digitalWrite(MOSFETpin,LOW);//make sure we are off
+  digitalWrite(MOSFETpin,HIGH);//make sure we are off
   colourCount=0;
   sleep();
 }
 
 void loop() {
   //digitalWrite(MOSFETpin,digitalRead(pressPin));
+  pixels.setPixelColor(0,getColour());
   if(digitalRead(pressPin)==LOW){
       pressCount++;
+      pixels.setBrightness(map(pressCount,0,3000,bright,0));//bright/2);// - ((bright/3000)*pressCount));
   }
   else{
     pressCount=0;
+    pixels.setBrightness(bright);
   }
   //show the battery status colour
-  pixels.setPixelColor(0,getColour());
   pixels.show();
   if (pressCount > 3000){
     pixels.setPixelColor(0,0,0,0);
@@ -47,20 +55,16 @@ void loop() {
 
 uint32_t getColour(){
   int iTemp;
-  //only read the voltage once every 10000 attempts
-  //if(colourCount>10000){
-    iTemp = readVcc()+readVcc()+readVcc()+readVcc()+readVcc();
-    iTemp = iTemp/5;//get the average
-    if(iTemp > 4200)
-      voltage = 255;
-    else if(voltage < 3300)
-      voltage = 0;
+  //get the average of 5 reads to increas stability
+  iTemp = readVcc()+readVcc()+readVcc()+readVcc()+readVcc();
+  iTemp = iTemp/5;//get the average
+  if(iTemp > 4200)//we are over 4.2v - bad things must have happened, just revert to full
+    voltage = 255;
+  else if(voltage < 3300)//revert anything below 3.3v to empty
+    voltage = 0;
+  else//anything else is fair game, lets report it
     voltage = map(iTemp,3300,4200,0,255);
-    //colourCount=0;
-  //}
-  //else{
-  //  colourCount++;
-  //}
+  
   return pixels.Color(255-voltage,voltage,0);
 }
 
@@ -91,7 +95,7 @@ long readVcc() {
 //https://bigdanzblog.wordpress.com/2014/08/10/attiny85-wake-from-sleep-on-pin-state-change-code-example/
 void sleep() {
     //make sure the FET is off
-    digitalWrite(MOSFETpin,LOW);
+    digitalWrite(MOSFETpin,HIGH);
     //and set the neopixel off so we save juice
     pixels.setPixelColor(0,0,0,0);
     pixels.show();
@@ -112,7 +116,7 @@ void sleep() {
     //sei();                                  // Enable interrupts
 
     //lets wait for debouncing
-    for(int i=0;i<10;i++){
+    for(int i=0;i<5;i++){
       //if(i%2==0){
       for(int j=0;j<bright;j++){
         pixels.setPixelColor(0,getColour());
@@ -143,6 +147,28 @@ void sleep() {
       digitalWrite(MOSFETpin,LOW);    
     }
 } // sleep
+
+//using code from here https://thewanderingengineer.com/2014/02/17/attiny-i2c-slave/
+// Gets called when the ATtiny receives an i2c request
+void requestEvent(){
+  //return the voltage to whatever asked for it
+  int iTemp;
+  //get the average of 5 reads to increas stability
+  iTemp = readVcc()+readVcc()+readVcc()+readVcc()+readVcc();
+  iTemp = iTemp/5;//get the average
+  iTemp = iTemp/100;//if out milivolts reports 4000, convert it to 40, so it can become 4.0v later
+  TinyWireS.send(iTemp);
+}
+
+void receiveEvent(){
+  //return the voltage to whatever asked for it
+  int iTemp;
+  //get the average of 5 reads to increas stability
+  iTemp = readVcc()+readVcc()+readVcc()+readVcc()+readVcc();
+  iTemp = iTemp/5;//get the average
+  iTemp = iTemp/100;//if out milivolts reports 4000, convert it to 40, so it can become 4.0v later
+  TinyWireS.send(iTemp);
+}
 
 ISR(PCINT0_vect) {
     // This is called when the interrupt occurs, but I don't need to do anything in it
